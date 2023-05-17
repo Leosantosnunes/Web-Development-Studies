dc1e54bcad0920b1b76e0937abd555c089d75bfb
Leosantosnunes <leonardon09@gmail.com>
2023-05-17 19:53:13 -0400

    let ans const

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 import doSomething from "./doSomething";
-import { square,halfOf,yourName } from "./lib";
+import {square,halfOf,yourName } from "./lib";
 
 
 console.log(squareValue(11));
 console.log(halfOf(10));
 doSomething();
 console.log(yourName);
+
+
+//let - It will allow you to change the value after initialization.
+//let - It will not allow you to change the value after initialization.
+let myName = "Leonardo";
+console.log(myName);
+myName = "Leonard";
+console.log(myName);
+
+const Name = "Maria";
+console.log(Name);
+Name = "Mari";
+console.log(Name);
