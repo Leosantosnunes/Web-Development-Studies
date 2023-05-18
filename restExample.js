558d9ce9d8c7f436956088061a0ae8ac39e26c14
Leosantosnunes <leonardon09@gmail.com>
2023-05-17 20:04:00 -0400

    rest example

---
+++ restExample.js
@@ -0,0 +1,7 @@
+function myStudents(professor,...students){
+    console.log("Profressor Name: " + professor);
+    console.log("Number of students: " + students.length);
+    console.log(students[1]);
+}
+
+myStudents("Krunal","jorge","Ishan","Laura")
