51aafeeb141ade576b0d3aa1c1406ec5c3b44dfd
Leosantosnunes <leonardon09@gmail.com>
2023-06-08 10:46:32 -0400

    fourth week

---
+++ Week04/express-app/models/product.js
@@ -0,0 +1,16 @@
+let mongoose = require('mongoose');
+
+//Create Model of Product
+
+let productModel = mongoose.Schema(
+    {
+        "name" : String,
+        "company" : String,
+        "price" : Number
+    },
+    {
+        collection : "product"
+    }
+);
+
+module.exports = mongoose.model('Product', productModel)
