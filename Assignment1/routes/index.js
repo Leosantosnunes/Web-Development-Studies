ce0698ec7f640c6a11879ac24dd77c723b366f6b
Leosantosnunes <leonardon09@gmail.com>
2023-06-09 11:09:50 -0400

    created home and about me page

--- Assignment1/routes/index.js
+++ Assignment1/routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about'); 
+});
+
+/* GET contact page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index'); 
+});
+
+/* GET project page. */
+router.get('/services', function(req, res, next) {
+  res.render('index'); 
+});
+
+/* GET Service page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index'); 
 });
 
 module.exports = router;
