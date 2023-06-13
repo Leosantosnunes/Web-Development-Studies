a1bf007094d91a50092dc0c41f5c1288fbbeae11
Leosantosnunes <leonardon09@gmail.com>
2023-06-13 19:56:26 -0400

    fixed errors, inserted animation, concluded project

--- Assignment1/routes/contact.js
+++ Assignment1/routes/contact.js
@@ -7,27 +7,33 @@ const fs = require('fs');
 router.post('/', (req, res, next) => {  
   const data = { "name":req.body.pname , 
                   "email":req.body.email, 
-                  "message":req.body.newmessage };
-  
-
-
-  
-  let jsonData = [];
-
-  jsonData.push(data);
-
-  const updatedData = JSON.stringify(jsonData);
-
-  // Save the JSON data to a file
-  fs.writeFile('data.json', updatedData, (err) => {
+                  "message":req.body.newmessage };  
+                   
+  fs.readFile('data.json', 'utf8', (err, fileData) => {
     if (err) {
       console.error(err);
-      res.status(500).send('Error saving data');
-    } else {
-      res.render('index');
-      res.send('Data saved successfully');
+      return res.status(500).send('Error reading data');
     }
-  });
+
+    let jsonData = [];
+    if (fileData) {
+      jsonData = JSON.parse(fileData); 
+    }
+    
+    jsonData.push(data); 
+
+    const updatedData = JSON.stringify(jsonData);
+
+    // Save the JSON data to a file
+    fs.writeFile('data.json', updatedData, (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Error saving data');
+      }
+
+    res.redirect('/');      
+    });
+  });  
 });
 
 module.exports = router;
